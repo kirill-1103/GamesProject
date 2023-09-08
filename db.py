722c184ee073a12a8ed2fcdf55e9894e9d7f085a
kirill kreysmann <kreysmann.kirya@mail.ru>
2023-09-08 21:42:27 +0300

    workflow1

---
+++ db.py
@@ -0,0 +1,16 @@
+import psycopg2
+conn = psycopg2.connect(dbname="games", user="kirill", password:"123" , host:"localhost")
+
+cursor = conn.cursor()
+
+cursor.execute("CREATE TABLE test (test1 varchar, test2 varchar)")
+
+cursor.execute("INSERT INTO test (test1, test2) VALUES ('1','2')")
+
+cursor.execute("SELECT * FROM test")
+
+print(cursor.fetchAll())
+
+cursor.close()
+
+conn.close()

a540ddfe15f1e22339355b20bc530665d245fd15
kirill kreysmann <kreysmann.kirya@mail.ru>
2023-09-09 15:34:47 +0300

    workflow

--- db.py
+++ db.py
@@ -86,7 +86,7 @@ player2_time INTEGER\
 );")
 
 cursor.execute("INSERT INTO player (login, email, password,role,  sign_up_time, last_sign_in_time, rating, photo, enabled)\
-VALUES ('kirill','k@mail.ru','$2a$10$M3.Ts0gg0Ldft6fB7N1tiusvTs5eR03inetM.uOQBMHux8BBRMSs2',\
+VALUES ('kirill','k@mail.ru','$2a$10$Tt8kBKpE3vpOyjb5sqARXuJiqYBcDinWz3OhvYVcRr6r5NmnWyn22',\
         'ROLE_USER',TIMESTAMP '2011-05-16 15:36:38',TIMESTAMP '2011-05-16 15:36:38',100,NULL,TRUE);")
 
 cursor.execute("INSERT INTO player (login, email, password,  role, sign_up_time, last_sign_in_time, rating, photo, enabled)\
